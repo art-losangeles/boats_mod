a7873834b64acf88f4371ecd80a91986f097e104
Thibaut GRIDEL <tgridel@free.fr>
2008-10-07 14:24:07 +0200

    Basic debug Level parsing

--- main.cpp
+++ main.cpp
@@ -9,13 +9,23 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 //
+#include <iostream>
+
 #include <QApplication>
 
 #include "mainwindow.h"
 
+int debugLevel = 0;
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+	int i;
+	QStringList arguments = QCoreApplication::arguments();
+	if (-1 != (i=arguments.indexOf("-debug"))) {
+		debugLevel = arguments[i+1].toInt();
+		std::cout << "debug level set to " << debugLevel << std::endl;
+	}
     // MainWindow
     MainWindow window;
     window.show();
